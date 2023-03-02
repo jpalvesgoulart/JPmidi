d8dde69a728206a02e1bed4f2d2185f663a75cea
João Pedro Alves Goulart <jpalves.dev@outlook.com>
2023-03-02 15:43:50 -0300

    Add files via upload

---
+++ Midi/main.js
@@ -0,0 +1,35 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else{
+        console.log('Elemento não encontrado ou seletor inválido.')
+    }
+
+
+}
+
+const listaTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaTeclas.length; i++) {
+
+    const tecla = listaTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (event) {
+        if (event.code == 'Space' || event.code == 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
